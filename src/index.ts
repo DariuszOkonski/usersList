003690d5b8927596e9e8be144910a00ba5d04cad
Dariusz Okoński <darek200180@gmail.com>
2025-09-25 07:44:55 +0200

    add index.ts

---
+++ src/index.ts
@@ -0,0 +1,19 @@
+import inquirer from 'inquirer';
+
+inquirer
+  .prompt([
+    {
+      name: 'name',
+      type: 'input',
+      message: "What's your name?",
+    },
+    {
+      name: 'age',
+      type: 'number',
+      message: 'How old are you?',
+      default: 18,
+    },
+  ])
+  .then((answers: { name: string; age: number }) => {
+    console.log(`\nHi ${answers.name}. ${answers.age}? Nice! \n`);
+  });
