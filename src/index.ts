73b23be49d7ee13d6130a2c51a76fcc2bd6a6de8
Dariusz Okoński <darek200180@gmail.com>
2025-09-25 08:02:17 +0200

    add class Message

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,61 @@
 import inquirer from 'inquirer';
 
-inquirer
-  .prompt([
-    {
-      name: 'name',
-      type: 'input',
-      message: "What's your name?",
-    },
-    {
-      name: 'age',
-      type: 'number',
-      message: 'How old are you?',
-      default: 18,
-    },
-  ])
-  .then((answers: { name: string; age: number }) => {
-    console.log(`\nHi ${answers.name}. ${answers.age}? Nice! \n`);
-  });
+enum Action {
+  List = 'list',
+  Add = 'add',
+  Remove = 'remove',
+  Quit = 'quit',
+}
+
+type InquirerAnswers = {
+  action: Action;
+};
+
+class Message {
+  constructor(private content: string) {}
+
+  static showColorized() {}
+
+  public show() {
+    console.log('content: ', this.content);
+  }
+
+  public capitalize() {
+    if (this.content.length === 0) return;
+    this.content =
+      this.content.charAt(0).toUpperCase() +
+      this.content.slice(1).toLowerCase();
+  }
+
+  public toUpperCase() {
+    if (this.content.length === 0) return;
+    this.content = this.content.toUpperCase();
+  }
+
+  public toLowerCase() {
+    if (this.content.length === 0) return;
+    this.content = this.content.toLowerCase();
+  }
+}
+
+const startApp = () => {
+  inquirer
+    .prompt([
+      {
+        name: 'action',
+        type: 'input',
+        message: 'How can I help you?',
+      },
+    ])
+    .then((answers: { action: Action }) => {
+      console.log('Chosen action: ' + answers.action);
+      if (answers.action === 'quit') {
+        console.log('I SHOULD QUIT');
+        return process.exit();
+      }
+
+      startApp();
+    });
+};
+
+startApp();
